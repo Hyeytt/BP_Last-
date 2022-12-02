835fbbdae5baef9525b1494302a2df2e2c7f7cb3
Hyeytt <112676923+Hyeytt@users.noreply.github.com>
2022-12-02 12:22:08 +0900

    2022년 12월 2일 기초파이썬 수업

---
+++ CH13.py
@@ -0,0 +1,25 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Traceback (most recent call last):
+  File "C:/Users/socpc22/Desktop/wewewewe.py", line 10, in <module>
+    t3.shape(square)
+NameError: name 'square' is not defined
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
+Traceback (most recent call last):
+  File "C:/Users/socpc22/Desktop/wewewewe.py", line 21, in <module>
+    t3.circle(200)
+NameError: name 't3' is not defined
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
